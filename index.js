4d238fc4b921394d9dbc4a454c18557728350865
Narankhuu Ganbaatar <narankhuuu@gmail.com>
2023-09-22 18:04:08 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+const catsCopy = [...cats];
+
+function appendCat(name) {
+    const catsCopy = [...cats]; 
+    catsCopy.push(name); 
+    return catsCopy; 
+ }
+function prependCat(name) {
+    const catsCopy = [...cats]; 
+    catsCopy.unshift(name); 
+    return catsCopy; 
+}
+function removeLastCat(name) {
+    const catsCopy = [...cats]; 
+    catsCopy.pop(name); 
+    return catsCopy; 
+}
+function removeFirstCat(name) {
+    const catsCopy = [...cats]; 
+    catsCopy.shift(name); 
+    return catsCopy; 
+}
